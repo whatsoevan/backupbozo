946a9de4f96c0fa540faeb868ae58551959efec8
Evan X <64918172+evanetizen@users.noreply.github.com>
2025-09-19 17:31:42 -0400

    restructure project

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"time"
 
@@ -17,6 +18,37 @@ import (
 	"github.com/spf13/cobra"
 	_ "modernc.org/sqlite"
 )
+
+// Estimated size per DB record and minimum DB padding for free space check
+const dbRecordEstimate = 512          // bytes per file record
+const dbMinPadding = 10 * 1024 * 1024 // 10 MB minimum padding
+
+// allowedExtensions defines which file types are considered for backup
+var allowedExtensions = map[string]bool{
+	".jpg":  true,
+	".jpeg": true,
+	".heic": true,
+	".png":  true,
+	".mp4":  true,
+	".mov":  true,
+	".mkv":  true,
+	".webm": true,
+	".avi":  true,
+}
+
+func estimateDBSize(numFiles int) int64 {
+	est := int64(numFiles) * dbRecordEstimate
+	if est < dbMinPadding {
+		return dbMinPadding
+	}
+	return est
+}
+
+// checkExternalTool checks if a tool is available in PATH
+func checkExternalTool(tool string) bool {
+	_, err := exec.LookPath(tool)
+	return err == nil
+}
 
 func main() {
 	var srcDir, destDir, dbPath, reportPath string
